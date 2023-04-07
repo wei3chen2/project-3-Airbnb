5bf1f310f95ce1f66684c16a7ee976b42d1d0b65
wei3chen2 <120448264+wei3chen2@users.noreply.github.com>
2023-04-07 01:54:24 -0700

    Add files via upload

--- Abmap.js
+++ Abmap.js
@@ -1,39 +1,89 @@
-
 var map = L.map('map', {
-  center: [22.28327, 20.1127],
-  zoom: 2.5
-});
-var basemap = { 
-  StreetView: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',   {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}),
-  Topography: L.tileLayer.wms('http://ows.mundialis.de/services/service?',   {layers: 'TOPO-WMS'}),
-  Places: L.tileLayer.wms('http://ows.mundialis.de/services/service?', {layers: 'OSM-Overlay-WMS'})
-};
+    center: [22.28327, 20.1127],
+    zoom: 2.5
+  });
+  
+  // Adding  3 basemap layers (the background map image) to our map:
+  
+  var basemap = { 
+    StreetView: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',   {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}),
+    Topography: L.tileLayer.wms('http://ows.mundialis.de/services/service?',   {layers: 'TOPO-WMS'}),
+    Places: L.tileLayer.wms('http://ows.mundialis.de/services/service?', {layers: 'OSM-Overlay-WMS'})
+  };
+  
+  L.control.layers(basemap).addTo(map);
+  
+  basemap.StreetView.addTo(map);
+  
+  var Abicon = L.icon({iconUrl: 'https://raw.githubusercontent.com/shacheeswadia/leaflet-map/main/beach-icon-colorful.svg', iconSize: [30, 30], })
+  
+  let cities = [
+    {    
+        name: "Hongkong",
+        listing: { EntireHomePrice : 'Entire Home Price $171',
+        PrivateRoomPrice : 'Private Room Price $262',
+        location: [22.28327, 114.14988]
+    },
+    },
+    {   name: "Paris",
+        listing: { EntireHomePrice : 'Entire Home Price $184',
+                PrivateRoomPrice : 'Private Room Price $140',
+                location: [48.85247, 2.35835]
+            },
+            },
+    
+  
+    {    
+        name: "Newyork",
+        listing: { EntireHomePrice : 'Entire Home Price $263',
+        PrivateRoomPrice : 'Private Room Price $166',
+        location: [40.75356, -73.98559]
+    },
+    },
+    {    
+        name: "London",
+        listing: { EntireHomePrice : 'Entire Home Price $278',
+        PrivateRoomPrice : 'Private Room Price $136',
+        location: [51.56861, -0.1127]
+    },
+    },
+    {
+        name: "Instanbul",
+        listing: { EntireHomePrice : 'Entire Home Price $147',
+        PrivateRoomPrice : 'Private Room Price $95',
+        location: [41.03254, 28.98153]
+    },
+    },
+    
+    {                
+        name: "Singapore",
+        listing: { EntireHomePrice : 'Entire Home Price $414',
+        PrivateRoomPrice : 'Private Room Price $158',
+        location: [1.34754, 103.95958]
+    },
+    },
 
-L.control.layers(basemap).addTo(map);
+    {   
+        name: "Bangkok",
+    listing: { EntireHomePrice : 'Entire Home Price $100',
+            PrivateRoomPrice : 'Private Room Price $88',
+            location: [13.75983, 100.54134]
+        },
+        },    
+];
 
-basemap.StreetView.addTo(map);
-
-var Abicon = L.icon({iconUrl: 'https://raw.githubusercontent.com/shacheeswadia/leaflet-map/main/beach-icon-colorful.svg', iconSize: [30, 30], })
+console.log(cities);   
 
 
-var marker1 = L.marker([22.28327, 114.14988], {icon: Abicon})
-            .bindPopup('Hongkong, China')
-            .addTo(map);
-var marker2 = L.marker([41.03254, 28.98153], {icon: Abicon})
-  .bindPopup('Instanbul, Turkey')
-  .addTo(map);
-var marker3 = L.marker([40.75356, -73.98559], {icon: Abicon})
-  .bindPopup('NewYork, United States')
-  .addTo(map);
-var marker4 = L.marker([51.56861, -0.1127], {icon: Abicon})
-  .bindPopup('London, England')
-  .addTo(map);
-var marker5 = L.marker([1.34754, 103.95958], {icon: Abicon})
-  .bindPopup('Singapore, Singapore')
-  .addTo(map);
-var marker6 = L.marker([48.85247, 2.35835], {icon: Abicon})
-  .bindPopup('Paris, France')
-  .addTo(map);
-var marker7 = L.marker([13.75983, 100.54134], {icon: Abicon})
-  .bindPopup('Bangkok, Thailand')
-  .addTo(map);
+ //looking through the list  
+
+    for (let i = 0; i < cities.length; i++) {
+
+        var city = cities[i];
+        console.log(city);
+        L.marker(city.listing.location, {icon: Abicon})
+            .bindPopup(`<h2>${city.name}</h2> <h5> ${city.listing.EntireHomePrice}</h5><h5> ${city.listing.PrivateRoomPrice}</h5>`)
+            .addTo(map)
+        
+    };
+
